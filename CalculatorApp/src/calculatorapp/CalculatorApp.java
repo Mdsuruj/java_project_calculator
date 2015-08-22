8f25ee1b0f2a1f878addb1d1b2dae2710331d244
Mdsuruj <surujantor@gmail.com>
2015-08-22 23:04:22 +0600

    Update CalculatorApp.java

--- CalculatorApp/src/calculatorapp/CalculatorApp.java
+++ CalculatorApp/src/calculatorapp/CalculatorApp.java
@@ -14,6 +14,6 @@ public class CalculatorApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Azizul Hakim");
+        System.out.println("suruj");
     }
 }
